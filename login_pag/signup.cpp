a32f8a1a2111c1ed8ab0e240974e4a35e9193bbf
amirsoleimani7 <amirsoleimani3131@gmail.com>
2023-12-16 22:34:18 +0330

    all parts in sign up cheack for being empty execpt gender combo box

--- login_pag/signup.cpp
+++ login_pag/signup.cpp
@@ -1,6 +1,7 @@
 #include "signup.h"
 #include "ui_signup.h"
-
+#include <QFile>
+#include <QMessageBox>
 signup::signup(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::signup)
@@ -12,3 +13,32 @@ signup::~signup()
 {
     delete ui;
 }
+
+void signup::on_pushButton_clicked()
+{
+    if(ui->lineEdit_user_name->text().isEmpty()){
+        QMessageBox::warning(this,"user name","user name is empty");
+    }
+
+    if(ui->lineEdit_password->text().isEmpty()){
+        QMessageBox::warning(this,"password","password is empty");
+    }
+
+    if(ui->lineEdit_name->text().isEmpty()){
+        QMessageBox::warning(this,"name","name is empty");
+    }
+
+    if(ui->lineEdit_email->text().isEmpty()){
+        QMessageBox::warning(this,"email","email is empty");
+    }
+
+    if(ui->lineEdit_age->text().isEmpty()){
+        QMessageBox::warning(this,"age","age is empty");
+    }
+    else{
+        QMessageBox::information(this,"reausalt","all good!!");
+    }
+
+
+}
+
